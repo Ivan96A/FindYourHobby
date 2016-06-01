e77c1fac83f1901ad977968fa59dab7df22d0149
Iwan <zobenko96@mail.ru>
2016-06-01 11:12:52 +0300

    added category music to db-init.sql

--- src/main/resources/db-initialization.sql
+++ src/main/resources/db-initialization.sql
@@ -32,6 +32,28 @@ values('До літнього виду спорту відноситься ...',
 insert into tasks(question, categories_id)
 values('До зимового виду спорту відноситься...', 1);
 
+insert into tasks(question, categories_id)
+values('Хто переміг на конкурсі євробачення 2006 року?',  2);
+insert into tasks(question,  categories_id)
+values('Який музичний стиль грає німецька група Ramstein',  2);
+insert into tasks(question,  categories_id)
+values('Яке місце посіла Україна на євробаченні 2013 року?',  2);
+insert into tasks(question,  categories_id)
+values('Скільки струн у стандартної акустичної гітари?',  2);
+insert into tasks(question,  categories_id)
+values('Які роки стали періодом набуття популярності групою Qeen?',  2);
+insert into tasks(question,  categories_id)
+values('У якому році було засновано гурт Metallica?',  2);
+insert into tasks(question,  categories_id)
+values('Яка з перечислених груп не входить в так звану «Велику четвірку трешметалу»?',  2);
+insert into tasks(question, categories_id)
+values('У якому році на телеекранах з’явився перший музичний кліп? ', 2);
+insert into tasks(question, categories_id)
+values('Хто з перечислених виконавців зібрав рекордну кількість глядачів?', 2);
+insert into tasks(question, categories_id)
+values('Яку пісню групи Parkway Drive фанати запропонували зробити гімном австралії?', 2);
+
+
 
 insert into answer_variants(answer, tasks_id)
 values('13', 1);
@@ -105,6 +127,97 @@ values('Тріатлон', 8);
 insert into answer_variants(answer, tasks_id)
 values('Веслування', 8);
 
+insert into answer_variants(answer, tasks_id)
+values('Руслана', 9);
+insert into answer_variants(answer, tasks_id)
+values('Lordi', 9);
+insert into answer_variants(answer, tasks_id)
+values('Анна Віссі', 9);
+insert into answer_variants(answer, tasks_id)
+values('Daz Sampson', 9);
+
+insert into answer_variants(answer, tasks_id)
+values('Heavy metal', 10);
+insert into answer_variants(answer, tasks_id)
+values('Hard rock', 10);
+insert into answer_variants(answer, tasks_id)
+values('Industrial metal', 10);
+insert into answer_variants(answer, tasks_id)
+values('Pop', 10);
+
+insert into answer_variants(answer, tasks_id)
+values('1', 11);
+insert into answer_variants(answer, tasks_id)
+values('2', 11);
+insert into answer_variants(answer, tasks_id)
+values('3', 11);
+insert into answer_variants(answer, tasks_id)
+values('4', 11);
+
+insert into answer_variants(answer, tasks_id)
+values('6', 12);
+insert into answer_variants(answer, tasks_id)
+values('7', 12);
+insert into answer_variants(answer, tasks_id)
+values('8', 12);
+insert into answer_variants(answer, tasks_id)
+values('9', 12);
+
+insert into answer_variants(answer, tasks_id)
+values('1960-ті', 13);
+insert into answer_variants(answer, tasks_id)
+values('1970-ті', 13);
+insert into answer_variants(answer, tasks_id)
+values('1980-ті', 13);
+insert into answer_variants(answer, tasks_id)
+values('1990-ті', 13);
+
+insert into answer_variants(answer, tasks_id)
+values('1980', 14);
+insert into answer_variants(answer, tasks_id)
+values('1986', 14);
+insert into answer_variants(answer, tasks_id)
+values('1979', 14);
+insert into answer_variants(answer, tasks_id)
+values('1981', 14);
+
+insert into answer_variants(answer, tasks_id)
+values('Metallica', 15);
+insert into answer_variants(answer, tasks_id)
+values('Antrax', 15);
+insert into answer_variants(answer, tasks_id)
+values('Vektor', 15);
+insert into answer_variants(answer, tasks_id)
+values('Megadeath', 15);
+
+insert into answer_variants(answer, tasks_id)
+values('1978', 16);
+insert into answer_variants(answer, tasks_id)
+values('1979', 16);
+insert into answer_variants(answer, tasks_id)
+values('1980', 16);
+insert into answer_variants(answer, tasks_id)
+values('1981', 16);
+
+insert into answer_variants(answer, tasks_id)
+values('Род Стюарт', 17);
+insert into answer_variants(answer, tasks_id)
+values('Курт Коббейн', 17);
+insert into answer_variants(answer, tasks_id)
+values('Qeen ', 17);
+insert into answer_variants(answer, tasks_id)
+values('Гарт Брукс', 17);
+
+insert into answer_variants(answer, tasks_id)
+values('Wild Eyes', 18);
+insert into answer_variants(answer, tasks_id)
+values('Writings on the walls', 18);
+insert into answer_variants(answer, tasks_id)
+values('Vice Grip', 18);
+insert into answer_variants(answer, tasks_id)
+values('Idols And Anchors', 18);
+
+
 
 update  tasks
 set correct_answer_id=3 where id=1;
@@ -122,6 +235,27 @@ update tasks
 set correct_answer_id=27 where id=7;
 update  tasks
 set correct_answer_id=29 where id=8;
+
+update  tasks
+set correct_answer_id=34 where id=9;
+update  tasks
+set correct_answer_id=39 where id=10;
+update  tasks
+set correct_answer_id=43 where id=11;
+update  tasks
+set correct_answer_id=45 where id=12;
+update  tasks
+set correct_answer_id=50 where id=13;
+update  tasks
+set correct_answer_id=56 where id=14;
+update  tasks
+set correct_answer_id=59 where id=15;
+update  tasks
+set correct_answer_id=62 where id=16;
+update  tasks
+set correct_answer_id=65 where id=17;
+update  tasks
+set correct_answer_id=71 where id=18;
 
 
 
