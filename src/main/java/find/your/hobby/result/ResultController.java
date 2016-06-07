594c01168f20c8f2db279bfe88c58d1bca32692b
Iwan <zobenko96@mail.ru>
2016-06-07 19:20:33 +0300

    fixed

--- src/main/java/find/your/hobby/result/ResultController.java
+++ src/main/java/find/your/hobby/result/ResultController.java
@@ -39,18 +39,6 @@ public class ResultController {
             produces = MediaType.APPLICATION_JSON_VALUE)
     public FullResult getResult()
     {
-        Result result1 = new Result(new Long(3),"");
-        Result result2 = new Result(new Long(12),"");
-        Result result3 = new Result(new Long(21),"");
-        Result result4 = new Result(new Long(70),"");
-        Result result5 = new Result(new Long(38),"");
-        Result result6 = new Result(new Long(50),"");
-        thisResults.add(result1);
-        thisResults.add(result2);
-        thisResults.add(result3);
-        thisResults.add(result4);
-        thisResults.add(result5);
-        thisResults.add(result6);
         return resultService.getResult(thisResults);
     }
 
